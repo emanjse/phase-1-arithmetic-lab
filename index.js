88a584687b5b8444f87cf7ba64f80bc50ddf453d
Eman Jamal <emanjse@gmail.com>
2024-03-05 19:59:03 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+
+// Challenge 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log("Result of multiply:", multiply);
+
+// Challenge 2
+let random = Math.floor(Math.random() * 100) + 1;
+console.log("Random integer:", random);
+
+// Challenge 3
+let num3 = 64;
+let num4 = 60;
+let mod = num3 % num4;
+console.log("Result of mod:", mod);
+
+// Challenge 4
+let max = Math.max(5,10, 20);
+console.log("Max number:", max);
